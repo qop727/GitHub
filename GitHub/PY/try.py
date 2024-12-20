c7efbc75578eaae724450573bc40dc19e270af74
Matěj H <qop727@gmail.com>
2024-12-20 13:35:44 +0100

    Add files via upload

---
+++ GitHub/PY/try.py
@@ -0,0 +1,9 @@
+muj_seznam = [1, 2, 3, 4]
+
+# Iterace pomocí for-cyklu
+for prvek in muj_seznam:
+    print(prvek)
+
+# Iterace s indexem
+for i, prvek in enumerate(muj_seznam):
+    print(f"Index {i}: {prvek}")
